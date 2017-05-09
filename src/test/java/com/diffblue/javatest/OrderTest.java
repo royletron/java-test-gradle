7394c78c272d737060b859581c506f791855a5c2
jgwilson42 <james.wilson@diffblue.com>
2017-05-09 12:55:39 +0100

    Aligned tests with java-demo

--- src/test/java/com/diffblue/javatest/OrderTest.java
+++ src/test/java/com/diffblue/javatest/OrderTest.java
@@ -1,6 +1,10 @@
 package com.diffblue.javatest;
 
+// Copyright 2016-2017 DiffBlue limited. All rights reserved.
+
 import com.diffblue.javatest.nestedobjects.subpackage.Order;
+import com.diffblue.javatest.nestedobjects.subpackage.Item;
+import com.diffblue.javatest.nestedobjects.User;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,7 +12,19 @@ public class OrderTest {
   @Test
   public void hasItem() {
     Order order = new Order();
+
     boolean result = order.hasItem();
-    Assert.assertEquals(result, false);
+    Assert.assertFalse(result);
   }
+
+  @Test
+  public void checkItemCost() {
+    Order order = new Order();
+    User user = new User(order);
+    Item item = new Item();
+
+    boolean result = user.checkItemCost(item);
+    Assert.assertFalse(result);
+  }
+
 }
